e95e5e16d35abbce96ed95e5c724b4e7f08ef03a
Dustyn <dustyn_gibb@hotmail.com>
2024-03-10 06:10:05 -0400

    Getting some introductory stuff done, app bar. Gonna need to read a lot of documentation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,31 +1,57 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
 @Composable
 @Preview
-fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
+fun app() {
 
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+
+        appContent()
+    }
+}
+
+@Composable
+fun appContent() {
+    MaterialTheme {
+        Column {
+            TopAppBar(
+                title = {
+                    Text("KVault") }, modifier =  Modifier.align(Alignment.CenterHorizontally), actions = {
+                IconButton(onClick = { /* Handle minimize */ }) {
+                    Icon(Icons.Filled.ArrowDropDown, contentDescription = "Minimize")
+                }
+                IconButton(onClick = { /* Handle maximize */ }) {
+                    Icon(Icons.Filled.Add, contentDescription = "Maximize")
+                }
+                IconButton(onClick = { /* Handle close */ }) {
+                    Icon(Icons.Filled.Close, contentDescription = "Close")
+                }
+            })
+            Column(
+                modifier = Modifier.padding(16.dp).fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text("Your application content goes here")
+            }
         }
     }
 }
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
-        App()
+        app()
     }
 }
